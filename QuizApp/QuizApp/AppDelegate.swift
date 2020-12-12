6e58881720bce13af83346b544f47ce4640d1f74
aaronmhr <aaronmhr@gmail.com>
2020-12-12 23:44:33 +0100

    Remove unused app launching

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -13,15 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let viewController = ResultsViewController(summary: "You got 1/2 correct", answers: [
-            PresentableAnswer(question: "Question", answer: "Answer", wrongAnswer: nil),
-            PresentableAnswer(question: "Question", answer: "Hell yeah!", wrongAnswer: "Hell no!")
-        ])
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = viewController
-        self.window = window
-        window.makeKeyAndVisible()
         return true
     }
 }
