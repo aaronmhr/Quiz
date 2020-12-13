d40eb106e94b853c3793f8b01a3d181a6c22b214
aaronmhr <aaronmhr@gmail.com>
2020-12-13 01:56:41 +0100

    `NavigationControllerRouter` kept kept track of view controllers that present questions

---
+++ QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
@@ -0,0 +1,40 @@
+//
+//  NavigationControllerRouterTest.swift
+//  QuizAppTests
+//
+//  Created by Aaron Huánuco on 13/12/20.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import XCTest
+import QuizEngine
+
+import UIKit
+
+class NavigationControllerRouter: Router {
+    private let navigationController: UINavigationController
+
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        navigationController.pushViewController(UIViewController(), animated: false)
+    }
+
+    func routeTo(result: Result<String, String>) {
+
+    }
+}
+
+
+class NavigationControllerRouterTest: XCTestCase {
+    func test_routeToQuestion_presentsQuestionController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+    }
+}
