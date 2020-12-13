3de6aaa31f855f42abb7daaff92722075f37232d
aaronmhr <aaronmhr@gmail.com>
2020-12-13 19:55:40 +0100

    Crash with our own message when the question asked has no defined options

--- QuizApp/QuizAppTests/iOSViewControllerTest.swift
+++ QuizApp/QuizAppTests/iOSViewControllerTest.swift
@@ -16,9 +16,12 @@ class iOSViewControllerFactory {
         self.options = options
     }
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
+        guard let options = self.options[question] else {
+            fatalError("Couldn't find options for question: \(question)")
+        }
         switch question {
         case .singleAnswer(let questionString):
-            return QuestionViewController(question: questionString, options: options[question]!, selection: answerCallback)
+            return QuestionViewController(question: questionString, options: options, selection: answerCallback)
         default:
             return UIViewController()
         }
@@ -26,16 +29,16 @@ class iOSViewControllerFactory {
 }
 
 class iOSViewControllerTest: XCTestCase {
-    func test_questionViewController_createsControllerWithQuestion() {
+    func test_questionViewController_singleAnswer_createsControllerWithQuestion() {
         let question = Question.singleAnswer("Q1")
         let options = ["A1", "A2"]
         let sut = iOSViewControllerFactory(options: [question: options])
-        let controller = sut.questionViewController(for: Question.singleAnswer("Q1"), answerCallback: { _ in }) as? QuestionViewController
+        let controller = sut.questionViewController(for: question, answerCallback: { _ in }) as? QuestionViewController
 
         XCTAssertEqual(controller?.question, "Q1")
     }
 
-    func test_questionViewController_createsControllerWithOptions() {
+    func test_questionViewController_singleAnswer_createsControllerWithOptions() {
         let question = Question.singleAnswer("Q1")
         let options = ["A1", "A2"]
         let sut = iOSViewControllerFactory(options: [question: options])
