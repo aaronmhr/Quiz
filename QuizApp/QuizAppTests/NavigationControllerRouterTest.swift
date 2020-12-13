b89aab3eea927b760800e33b2fff02b48562eba6
aaronmhr <aaronmhr@gmail.com>
2020-12-13 01:59:12 +0100

    `NavigationControllerRouter` keeps state of how many times the `Router` routes

--- QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
@@ -37,4 +37,14 @@ class NavigationControllerRouterTest: XCTestCase {
 
         XCTAssertEqual(navigationController.viewControllers.count, 1)
     }
+
+    func test_routeToQuestionTwice_presentsQuestionController() {
+        let navigationController = UINavigationController()
+        let sut = NavigationControllerRouter(navigationController)
+
+        sut.routeTo(question: "Q1", answerCallback: { _ in })
+        sut.routeTo(question: "Q2", answerCallback: { _ in })
+
+        XCTAssertEqual(navigationController.viewControllers.count, 2)
+    }
 }
