fb5c5a58bc6d7ba3daf0ce63c37caddbe8023f4a
aaronmhr <aaronmhr@gmail.com>
2020-12-14 20:48:45 +0100

    Move `iOSViewControllerFactory` to its own file

--- QuizApp/QuizAppTests/iOSViewControllerTest.swift
+++ QuizApp/QuizAppTests/iOSViewControllerTest.swift
@@ -9,33 +9,6 @@
 import XCTest
 @testable import QuizApp
 
-class iOSViewControllerFactory {
-    private let options: [Question<String>: [String]]
-
-    init(options: [Question<String>: [String]]) {
-        self.options = options
-    }
-    func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
-        guard let options = self.options[question] else {
-            fatalError("Couldn't find options for question: \(question)")
-        }
-        return questionViewController(for: question, options: options, answerCallback: answerCallback)
-    }
-
-    private func questionViewController(for question: Question<String>, options: [String], answerCallback: @escaping ([String]) -> Void) -> UIViewController {
-        switch question {
-        case .singleAnswer(let value):
-            return QuestionViewController(question: value, options: options, selection: answerCallback)
-            
-        case .multipleAnswer(let value):
-            let viewController = QuestionViewController(question: value, options: options, selection: answerCallback)
-            viewController.loadViewIfNeeded()
-            viewController.tableView.allowsMultipleSelection = true
-            return viewController
-        }
-    }
-}
-
 class iOSViewControllerTest: XCTestCase {
     let options = ["A1", "A2"]
 
