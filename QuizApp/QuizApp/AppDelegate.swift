ba3ef1be8a03a1ddb393c08222a48634854c33c9
aaronmhr <aaronmhr@gmail.com>
2020-12-15 22:03:37 +0100

    Assemble the app

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -12,8 +12,39 @@ import QuizEngine
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var game: Game<Question<String>, Set<String>, NavigationControllerRouter>?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let question1 = Question.singleAnswer("What's Mike's nationality?")
+        let question2 = Question.multipleAnswer("What's Caio's nationalities?")
+
+        let questions = [question1, question2]
+
+        let option1 = "Canadian"
+        let option2 = "American"
+        let option3 = "Greek"
+
+        let option4 = "Portuguese"
+        let option5 = "American"
+        let option6 = "Brazilian"
+
+        let options = [
+            question1: [option1, option2, option3],
+            question2: [option4, option5, option6],
+        ]
+
+        let correctAnswers = [question1: Set([option3]), question2: [option4, option6]]
+
+        let navigationController = UINavigationController()
+        let factory = iOSViewControllerFactory(questions: questions, options: options, correctAnswers: correctAnswers)
+        let router = NavigationControllerRouter(navigationController, factory: factory)
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
+        game = startGame(questions: questions, router: router, correctAnswers: correctAnswers)
         return true
     }
 }
