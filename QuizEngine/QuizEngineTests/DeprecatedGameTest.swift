0e02f9cdba7609a64e174464a8447f1b4adb5e33
aaronmhr <aaronmhr@gmail.com>
2020-12-19 19:00:42 +0100

    Deprecate the integrated tests for the old APIs

--- QuizEngine/QuizEngineTests/DeprecatedGameTest.swift
+++ QuizEngine/QuizEngineTests/DeprecatedGameTest.swift
@@ -9,7 +9,8 @@
 import XCTest
 import QuizEngine
 
-class GameTest: XCTestCase {
+@available(*, deprecated)
+class DeprecatedGameTest: XCTestCase {
 
     let router = RouterSpy()
     var game: Game<String, String, RouterSpy>!
