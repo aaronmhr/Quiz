6463afc1273bfd23b60c55d22c85c7ebcdcb2151
aaronmhr <aaronmhr@gmail.com>
2020-12-19 01:02:29 +0100

    Move `QuizDelegate` to its own file

--- QuizEngine/QuizEngine/Router.swift
+++ QuizEngine/QuizEngine/Router.swift
@@ -6,13 +6,7 @@
 //  Copyright © 2020 Aaron Huánuco. All rights reserved.
 //
 
-public protocol QuizDelegate {
-    associatedtype Answer
-    associatedtype Question: Hashable
 
-    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
-    func handle(result: Result<Question, Answer>)
-}
 
 @available(*, deprecated)
 public protocol Router {
