2015e576fcc011ae563ac9435ecd018e62ecc74f
aaronmhr <aaronmhr@gmail.com>
2020-12-20 00:36:05 +0100

    Rename method to clarify intent

--- QuizEngine/QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizEngine/QuizDelegate.swift
@@ -10,6 +10,9 @@ public protocol QuizDelegate {
     associatedtype Question: Hashable
     associatedtype Answer
 
-    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+//    func answer(for question: Question, completion: @escaping (Answer) -> Void)
+//    func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)])
+
+    func answer(for question: Question, completion: @escaping (Answer) -> Void)
     func handle(result: Result<Question, Answer>)
 }
