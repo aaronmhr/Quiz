68e5440f6a6f0ec63b6916cf3f2f88784db42289
aaronmhr <aaronmhr@gmail.com>
2020-12-20 20:25:07 +0100

    Prove the score function allows to compare uneven data (too many correct answers)

--- QuizEngine/QuizEngineTests/ScoreTest.swift
+++ QuizEngine/QuizEngineTests/ScoreTest.swift
@@ -46,6 +46,14 @@ class ScoreTest: XCTestCase {
         XCTAssertEqual(score, 2)
     }
 
+    func test_withTooManyAnswers_oneMatchingAnswer_scoresOne() {
+        let score = BasicScore.score(
+            for: ["not matching answer", "another answer"],
+            comparingTo: ["an answer", "another answer", "an extra answer"])
+
+        XCTAssertEqual(score, 1)
+    }
+
     private class BasicScore {
         static func score(for answers: [String], comparingTo correctAnswers: [String] = []) -> Int {
             var score = 0
