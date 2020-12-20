a88e6f5c2d9c903a3019ce05363935a4b81efc3d
aaronmhr <aaronmhr@gmail.com>
2020-12-20 19:38:53 +0100

    Remove redundant code

--- QuizEngine/QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizEngine/QuizDelegate.swift
@@ -10,15 +10,7 @@ public protocol QuizDelegate {
     associatedtype Question: Hashable
     associatedtype Answer
 
-//    func answer(for question: Question, completion: @escaping (Answer) -> Void)
-
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
+
     func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)])
-
-    @available(*, deprecated, message: "use didCompleteQuiz(withAnswers:) instead")
-    func handle(result: Result<Question, Answer>)
-}
-
-public extension QuizDelegate {
-    func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)]) {}
 }
