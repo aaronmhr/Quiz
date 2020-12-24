791f342183864d840f685019e524f7f9208088c7
aaronmhr <aaronmhr@gmail.com>
2020-12-24 01:57:42 +0100

    Migrate AppDelegate

--- QuizApp/QuizApp/Main/AppDelegate.swift
+++ QuizApp/QuizApp/Main/AppDelegate.swift
@@ -12,7 +12,7 @@ import QuizEngine
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var game: Game<Question<String>, [String], NavigationControllerRouter>?
+    var quiz: Quiz?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
@@ -24,22 +24,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let option1 = "Canadian"
         let option2 = "American"
         let option3 = "Greek"
+        let options1 = [option1, option2, option3]
 
         let option4 = "Portuguese"
         let option5 = "American"
         let option6 = "Brazilian"
+        let options2 = [option4, option5, option6]
 
-        let options = [
-            question1: [option1, option2, option3],
-            question2: [option4, option5, option6],
-        ]
-
-        let correctAnswers = [question1: [option3], question2: [option4, option6]]
+        let options = [question1: options1, question2: options2]
+        let correctAnswers = [(question1, [option3]), (question2, [option4, option6])]
 
         let navigationController = UINavigationController()
         let factory = iOSViewControllerFactory(
             options: options,
-            correctAnswers: [(question1, [option3]), (question2, [option4, option6])]
+            correctAnswers: correctAnswers
         )
         let router = NavigationControllerRouter(navigationController, factory: factory)
 
@@ -47,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
-        game = startGame(questions: questions, router: router, correctAnswers: correctAnswers)
+        quiz = Quiz.start(questions: questions, delegate: router)
         return true
     }
 }
