ce2bfa4ab05cac74c2af199afb0a22177a05551c
aaronmhr <aaronmhr@gmail.com>
2020-12-25 19:32:16 +0100

    Notify handler with selected option on submit

--- QuizApp/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ QuizApp/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -10,12 +10,20 @@ import XCTest
 
 struct MultipleSelectionStore {
     var options: [MultipleSelectionOption]
+    private let handler: ([String]) -> Void
+
+    init(options: [String], handler: @escaping ([String]) -> Void) {
+        self.options = options.map { MultipleSelectionOption(text: $0) }
+        self.handler = handler
+    }
+
     var canSubmit: Bool {
         !options.filter(\.isSelected).isEmpty
     }
 
-    init(options: [String]) {
-        self.options = options.map { MultipleSelectionOption(text: $0) }
+    func submit() {
+        guard canSubmit else { return }
+        handler(options.filter(\.isSelected).map(\.text))
     }
 }
 
@@ -30,7 +38,7 @@ struct MultipleSelectionOption {
 
 class MultipleSelectionStoreTests: XCTestCase {
     func test_selectOption_togglesState() {
-        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        var sut = makeSUT(options: ["o0", "o1"])
         XCTAssertFalse(sut.options[0].isSelected)
 
         sut.options[0].select()
@@ -41,7 +49,7 @@ class MultipleSelectionStoreTests: XCTestCase {
     }
 
     func test_canSubmit_whenAtLeastOneOptionIsSelected() {
-        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        var sut = makeSUT(options: ["o0", "o1"])
         XCTAssertFalse(sut.canSubmit)
 
         sut.options[0].select()
@@ -52,5 +60,29 @@ class MultipleSelectionStoreTests: XCTestCase {
 
         sut.options[1].select()
         XCTAssertTrue(sut.canSubmit)
+    }
+
+    func test_submit_notifiesHandlerWithSelectedOptions() {
+        var submittedOptions = [[String]]()
+        var sut = makeSUT(options: ["o0", "o1"], handler: {
+            submittedOptions.append($0)
+        })
+
+        sut.submit()
+        XCTAssertEqual(submittedOptions, [])
+
+        sut.options[0].select()
+        sut.submit()
+        XCTAssertEqual(submittedOptions, [["o0"]])
+
+
+        sut.options[1].select()
+        sut.submit()
+        XCTAssertEqual(submittedOptions, [["o0"], ["o0", "o1"]])
+    }
+
+    // MARK: - Helpers
+    private func makeSUT(options: [String] = [], handler: @escaping ([String]) -> Void = { _ in }) -> MultipleSelectionStore {
+        MultipleSelectionStore(options: options, handler: handler)
     }
 }
