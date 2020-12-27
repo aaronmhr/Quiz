e35e495b1d0ba1cf63398550a45e248a86b2514b
aaronmhr <aaronmhr@gmail.com>
2020-12-27 01:41:44 +0100

    Move QuizApp to its own file

--- QuizApp/QuizApp/Main/AppDelegate.swift
+++ QuizApp/QuizApp/Main/AppDelegate.swift
@@ -7,36 +7,7 @@
 //
 
 import UIKit
-import SwiftUI
 import QuizEngine
-
-class AppStore {
-    var quiz: Quiz?
-}
-
-@main
-struct QuizApp: App {
-    let appStore = AppStore()
-    @StateObject var navigationStore = QuizNavigationStore()
-
-    var body: some Scene {
-        WindowGroup {
-            QuizNavigationView(store: navigationStore)
-                .onAppear(perform: startNewQuiz)
-        }
-    }
-
-    private func startNewQuiz() {
-        let adapter = iOSSwiftUINavigationAdapter(
-            navigation: navigationStore,
-            options: options,
-            correctAnswers: correctAnswers,
-            playAgain: startNewQuiz
-        )
-
-        appStore.quiz = Quiz.start(questions: questions, delegate: adapter)
-    }
-}
 
 //@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
